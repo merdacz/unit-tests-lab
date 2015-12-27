81d4c416dc4ff39d530d8041a18e299deebcaeed
Marcin Daczkowski <merdacz@ais.pl>
2015-12-27 10:46:16 +0100

    refactored main routine content into methods
    while the main rotine is kept out of obscure code and we
    can get the list of tests in our suite in the individual
    test case scenarios we still have lots of duplication. if anything
    needs to be changed in the future there we would need to apply this to
    every single test case.

--- console/Program.cs
+++ console/Program.cs
@@ -8,8 +8,15 @@
     {
         static void Main(string[] args)
         {
+            Test_Suggest_gear_decrease_upon_low_rpm();
+            Test_Suggest_gear_increase_upon_high_rpm();
+        }
+
+        private static void Test_Suggest_gear_decrease_upon_low_rpm()
+        {
             Console.Write("Test #1: Suggest gear decrease upon low rpm");
-            try {
+            try
+            {
                 var sut = new GearPrompter();
                 var result = sut.Recommend(new PrompterInput(2, 1000));
                 if (result.NextGear != 1)
@@ -23,9 +30,13 @@
             }
 
             Console.WriteLine();
+        }
 
+        private static void Test_Suggest_gear_increase_upon_high_rpm()
+        {
             Console.Write("Test #2: Suggest gear increase upon high rpm");
-            try {
+            try
+            {
                 var sut = new GearPrompter();
                 var result = sut.Recommend(new PrompterInput(1, 3000));
                 if (result.NextGear != 2)
