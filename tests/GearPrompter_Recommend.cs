0f52eaae2656e39582c472c6701959babbaa0809
Marcin Daczkowski <merdacz@ais.pl>
2015-12-27 11:14:17 +0100

    created initial test project
    Notice how xunit test "Facts" resemble scenarios we ended up after
    introducing test helpers. The main difference being how assert look like,
    which is due to specific framework being used for those. No custom
    infrastructure code is needed, all is being provided by XUnit and
    FluentAssetions libratries.
    
    Project also includes package for integration with VS testing explorer,
    so you can easily run the whole suite via Test -> Run -> All Tests
    or by hittng Ctrl-R, A. Additional benefit of using the framework is
    ability to run individual tests or groups of tests without code
    modification. Easiest way to do this is to use Test Explorer, which a
    automatically recognizes all your tests.
    
    If you are using VS 2015 you should also check Code Lenses (grayed out
    details displaying in between [Fact] attribute and the method header. By
    clicking red X icon you can get additional per case details including
    assertion failure details as well as execution time.

---
+++ tests/GearPrompter_Recommend.cs
@@ -0,0 +1,27 @@
+﻿namespace tests
+{
+    using FluentAssertions;
+
+    using library;
+
+    using Xunit;
+
+    public class GearPrompter_Recommend
+    {
+        [Fact]
+        public void Suggest_gear_decrease_upon_low_rpm()
+        {
+            var sut = new GearPrompter();
+            var result = sut.Recommend(new PrompterInput(2, 1000));
+            result.NextGear.Should().Be(1);
+        }
+
+        [Fact]
+        public void Suggest_gear_increase_upon_high_rpm()
+        {
+            var sut = new GearPrompter();
+            var result = sut.Recommend(new PrompterInput(1, 3000));
+            result.NextGear.Should().Be(2);
+        }
+    }
+}
