2438bccd417a5ae848007c47bab1b0cc26a6010b
Marcin Daczkowski <merdacz@ais.pl>
2015-12-27 10:54:51 +0100

    extracted generic test code into separate method
    Actual test scenarios are now much cleaner but still have assertion
    logic in there. We will be getting rid of that in the next steps
    but first we will be refactoring common code into separate TestHelper
    class.

--- console/Program.cs
+++ console/Program.cs
@@ -1,28 +1,27 @@
 ﻿namespace console
 {
     using System;
+    using System.Reflection;
 
     using library;
 
     class Program
     {
+        private static int counter = 1;
+
         static void Main(string[] args)
         {
-            Test_Suggest_gear_decrease_upon_low_rpm();
-            Test_Suggest_gear_increase_upon_high_rpm();
+            Test(Suggest_gear_decrease_upon_low_rpm);
+            Test(Suggest_gear_increase_upon_high_rpm);
         }
 
-        private static void Test_Suggest_gear_decrease_upon_low_rpm()
+        private static void Test(Action body)
         {
-            Console.Write("Test #1: Suggest gear decrease upon low rpm");
+            var scenario = body.GetMethodInfo().Name;
+            Console.Write($"Test #{counter}: {scenario}");
             try
             {
-                var sut = new GearPrompter();
-                var result = sut.Recommend(new PrompterInput(2, 1000));
-                if (result.NextGear != 1)
-                {
-                    Console.Write($" --> Assert failed: Expected 1 but got {result.NextGear}");
-                }
+                body();
             }
             catch (Exception ex)
             {
@@ -30,26 +29,27 @@
             }
 
             Console.WriteLine();
+            counter++;
         }
 
-        private static void Test_Suggest_gear_increase_upon_high_rpm()
+        private static void Suggest_gear_decrease_upon_low_rpm()
         {
-            Console.Write("Test #2: Suggest gear increase upon high rpm");
-            try
+            var sut = new GearPrompter();
+            var result = sut.Recommend(new PrompterInput(2, 1000));
+            if (result.NextGear != 1)
             {
-                var sut = new GearPrompter();
-                var result = sut.Recommend(new PrompterInput(1, 3000));
-                if (result.NextGear != 2)
-                {
-                    Console.Write($" --> Assert failed: Expected 1 but got {result.NextGear}");
-                }
+                Console.Write($" --> Assert failed: Expected 1 but got {result.NextGear}");
             }
-            catch (Exception ex)
-            {
-                Console.Write($" --> Unexpected exception: '{ex.Message}'");
-            }
+        }
 
-            Console.WriteLine();
+        private static void Suggest_gear_increase_upon_high_rpm()
+        {
+            var sut = new GearPrompter();
+            var result = sut.Recommend(new PrompterInput(1, 3000));
+            if (result.NextGear != 2)
+            {
+                Console.Write($" --> Assert failed: Expected 1 but got {result.NextGear}");
+            }
         }
     }
 }
